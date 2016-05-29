14ad3fffa8ccea3fbc9e6dd89e5dc16605f09ecf
Hessam Mehr <hessammehr@gmail.com>
2016-05-29 01:52:31 -0700

    Problem 4 added.

---
+++ prob4.ml
@@ -0,0 +1,11 @@
+let rec length = function [] -> 0
+                        | x::xs -> 1 + length xs
+
+let print_int x = print_endline (string_of_int x)
+
+let main () =
+  print_int (length []);
+  print_int (length [1]);
+  print_int (length [1,2])
+
+let _ = main ()
