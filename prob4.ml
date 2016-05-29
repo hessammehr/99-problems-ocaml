48e31c42ecc1019551ddbeeee23f40b109ac475d
Hessam Mehr <hessammehr@gmail.com>
2016-05-29 01:54:46 -0700

    Correct list constructor in test.

--- prob4.ml
+++ prob4.ml
@@ -6,6 +6,6 @@ let print_int x = print_endline (string_of_int x)
 let main () =
   print_int (length []);
   print_int (length [1]);
-  print_int (length [1,2])
+  print_int (length [1;2])
 
 let _ = main ()
