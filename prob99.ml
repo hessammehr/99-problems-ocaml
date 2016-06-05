f432d1f365095300afd575428c50b92ca0c712ab
Hessam Mehr <hessammehr@gmail.com>
2016-06-05 01:56:44 -0400

    ...

--- prob99.ml
+++ prob99.ml
@@ -1,32 +1,30 @@
-(* open Core.Std *)
+open Core.Std
 
-(* type cell = Black *)
-(*           | Empty *)
-(*           | Filled of char *)
+type cell = Black
+          | Empty
+          | Filled of char
 
-(* type coord = {r:int; c:int} *)
+type coord = {r:int; c:int}
 
-(* type puzzle = {cells:cell array; size:coord} *)
+type puzzle = {cells:cell array; size:coord}
 
-(* type direction = Horizontal | Vertical *)
+type direction = Horizontal | Vertical
 
-(* type cursor = {p:puzzle; start:coord; d:direction} *)
+type cursor = {p:puzzle; start:coord; d:direction}
 
-(* type range = {c:cursor; len:int} *)
+type range = {c:cursor; len:int}
 
-(* let rows puz n = *)
-(*   let {r;c} = puz.size in *)
-(*   let idxs =  *)List.range ~stride:c 0 (r*c) in
+let rows puz n =
+  let {r;c} = puz.size in
+  let idxs =  List.range ~stride:c 0 (r*c) in
   List.map ~f:(fun i->{p=puz; start={r=i; c=0}; d=Horizontal}) idxs
-  (* {p:puz; start:{r;c}; d:Horizontal} *)
 
-let get_cur cur n =
+let get cur n =
   match cur.d with
-  Horizontal -> Array.get cur.p ()
+  Horizontal -> None
+  | Vertical -> None
 
 let analyze puz = None
-
-let get p r c = Array.get p.cells (c+r*p.w)
 
 let len curs = None
 
